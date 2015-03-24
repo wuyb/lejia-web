00cc0f2789e7318735b7091d0cd0699c11b1ff8f
Yanbo Wu <yanbo.wu@gmail.com>
2015-03-25 04:43:17 +0800

    clean up log

--- app/scripts/controllers/video.js
+++ app/scripts/controllers/video.js
@@ -26,7 +26,6 @@ angular.module('webApp')
         name: 'videoFilter',
         fn: function(item /*{File|FileLikeObject}*/, options) {
             var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-            console.log(type);
             return '|x-msvideo|x-ms-wmv|quicktime|mp4|avi|mpeg|'.indexOf(type) !== -1;
         }
     });
@@ -49,17 +48,10 @@ angular.module('webApp')
         console.info('onWhenAddingFileFailed', item, filter, options);
     };
     uploader.onAfterAddingFile = function(fileItem) {
-        console.info('onAfterAddingFile', fileItem);
         $scope.upload(fileItem);
         $scope.item = fileItem;
         $scope.uploading = true;
         $scope.loading = true;
-    };
-    uploader.onBeforeUploadItem = function(item) {
-        console.info('onBeforeUploadItem', item);
-        console.log(JSON.stringify(item.formData));
-    };
-    uploader.onProgressItem = function(fileItem, progress) {
     };
     uploader.onSuccessItem = function(fileItem, response, status, headers) {
         console.info('onSuccessItem', fileItem, response, status, headers);
@@ -74,13 +66,11 @@ angular.module('webApp')
         console.info('onErrorItem', fileItem, response, status, headers);
     };
     uploader.onCancelItem = function(fileItem, response, status, headers) {
-        console.info('onCancelItem', fileItem, response, status, headers);
         $scope.item = null;
         $scope.uploading = false;
         $scope.loading = false;
     };
     uploader.onCompleteItem = function(fileItem, response, status, headers) {
-        console.info('onCompleteItem', fileItem, response, status, headers);
         $scope.resetFileSelector();
     };
 
